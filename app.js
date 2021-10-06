17b20bf67434b1681063a441609a47a57f13105a
Ubuntu <ubuntu@ip-172-31-16-255.us-east-2.compute.internal>
2021-10-06 08:45:35 +0000

    new change in app.js

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ const app = express();
 
 
 app.get('/', (req, res) => {
-    res.send('Welcome to the Homepage');
+    res.send('Welcome to the Node app deployed in AWS');
 })
 
 
-const port = process.env.port || 3000;
+const port = process.env.port || 5000;
 app.listen( port, () => {
     console.log("Hello Express")
 });
